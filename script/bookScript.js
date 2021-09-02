940625de9f6fe85368fdf2017421f38671a10a71
Mrinal-KANTI55 <baust.cse.170101055@gmail.com>
2021-09-02 15:14:35 +0600

    finish :)

--- script/bookScript.js
+++ script/bookScript.js
@@ -1,59 +1,63 @@
 //take book name from user
 const getBookName = () => {
-    const bookName = document.getElementById('searchText').value;
-    if(!bookName){
-      alert('please enter book name')
-    }else{
-      document.getElementById('searchText').value = '';
-      return searchUrl(bookName);
-    }
+  const bookName = document.getElementById('searchText').value;
+  if (!bookName) {
+    alert('please enter book name')
+  } else {
+    document.getElementById('searchText').value = '';
+    return searchUrl(bookName);
+  }
 }
 //fetch the url for search the book
 const searchUrl = searchText => {
-    fetch(`https://openlibrary.org/search.json?q=${searchText}`)
-        .then(response => response.json())
-        .then(data => showBookInfo(data, data.docs));
+  fetch(`https://openlibrary.org/search.json?q=${searchText}`)
+    .then(response => response.json())
+    .then(data => showBookInfo(data, data.docs));
 }
 //use it for show massage or not
-const visibleMassage=showOrNot=>document.getElementById('noResult').style.display=showOrNot;
+const visibleMassage = showOrNot => document.getElementById('noResult').style.display = showOrNot;
 //use it for search authors name
 const author_names = name => {
-  if(typeof(name)==='undefined'){
-    return name='Unknown author';
-  }else{
+  if (typeof (name) === 'undefined') {
+    return name = 'Unknown author';
+  } else {
     return name;
   }
 };
 //use it for search publish year 
-const publishYear= year=>{
-  if(typeof(year)==='undefined'){
-    return year='Unknown year';
-  }else{
+const publishYear = year => {
+  if (typeof (year) === 'undefined') {
+    return year = 'Unknown year';
+  } else {
     return year;
   }
 }
-const coverImage=image=>{
-  if(typeof(image)==='undefined'){
-    return image=10909258;
-  }else{
+// use it for default image if not found
+const coverImage = image => {
+  if (typeof (image) === 'undefined') {
+    return image = 10909258;
+  } else {
     return image;
   }
 }
+//here it shows all books information
 const showBookInfo = (count, info) => {
-    visibleMassage('none');
-    const bookBlock = document.getElementById('showDetail');
-    bookBlock.textContent = '';
-    document.getElementById('totalFound').innerText=count.numFound;   
-    if(info.length===0){
-      visibleMassage('block');
-      document.getElementById('noResult').innerText='No Result Found';
-    }  
-    info?.forEach(data => {
-        const div = document.createElement('div');
-        const authorNames = author_names(data.author_name);
-        const firstPublishYear =publishYear(data.first_publish_year);
-        const cover_i =coverImage(data.cover_i);
-        div.innerHTML = `
+  visibleMassage('none');
+  const bookBlock = document.getElementById('showDetail');
+  bookBlock.textContent = '';
+  document.getElementById('totalFound').innerText = count.numFound;
+  //here use for if search book is not found 
+  if (info.length === 0) {
+    visibleMassage('block');
+    document.getElementById('noResult').innerText = 'No Result Found';
+  }
+  //here it set the property for each book like image,name,time etc
+  info?.forEach(data => {
+    const div = document.createElement('div');
+    const authorNames = author_names(data.author_name);
+    const firstPublishYear = publishYear(data.first_publish_year);
+    const cover_i = coverImage(data.cover_i);
+    div.innerHTML = `
         <div class="card mb-3" style="max-width: 540px;">
                 <div class="row g-0">
                   <div class="col-md-4">
@@ -69,6 +73,6 @@ const showBookInfo = (count, info) => {
                 </div>
               </div>
         `;
-        bookBlock.appendChild(div);
-    });
+    bookBlock.appendChild(div);
+  });
 }
